d322cc0a86be9899e482c0234f2b0bfe85b314da
gr1101 <gauravrai110@gmail.com>
2025-01-25 15:00:18 +0530

    first commit

---
+++ src/main/java/devopsmyproj/Mycalculator.java
@@ -0,0 +1,21 @@
+package devopsmyproj;
+
+public class Mycalculator {
+	
+	public int sum(int a,int b) {
+
+	return a+b;
+}
+	
+	public int diff(int a, int b) {
+		return a-b;
+	}
+
+public static void main(String[] args) {
+	
+	Mycalculator calc = new Mycalculator();
+	System.out.println("Sum is "+calc.sum(20,10));
+	System.out.println("difference is "+calc.diff(20,10));
+	
+	}
+}
